0b98ae759f86fad6df41c175ebfa2708efa29c53
Adriana Abreu Mota Wutke <adriana7089@DIGITALCOLLEGE.LOCAL>
2024-05-09 12:03:52 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "primereat/resources/themes/lara-light-amber/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+
+const App = () => {
+  return ( 
+    <>
+    
+    </>
+   );
+}
+ 
+export default App;
